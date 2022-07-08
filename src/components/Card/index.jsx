81154f2ad2c66e6e1b69bd7018c2d2395356f8c5
anfezuar <anfezuar27_08@hotmail.com>
2022-07-07 20:53:09 -0500

    fix select card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,13 +3,24 @@ import React from "react";
 import check from "../../assets/check.png";
 
 import "./Card.css";
-function Card({ title, image, handlePressApp, selected = false }) {
+function Card({
+  title,
+  image,
+  handlePressApp,
+  selected = false,
+  versiones = [],
+}) {
+  const optionsRender = () =>
+    versiones.map((version) => <option value={version}>{version}</option>);
   return (
-    <button className="card-container" onClick={() => handlePressApp(title)}>
-      <h4>{title}</h4>
-      <img src={image} alt={"logo del programa"} />
-      {selected && <img src={check} alt={"check"} />}
-    </button>
+    <div className="card-container">
+      <button className="button-card" onClick={() => handlePressApp(title)}>
+        <h4>{title}</h4>
+        <img src={image} alt={"logo del programa"} className="logo" />
+        {selected && <img src={check} alt={"check"} className="check" />}
+      </button>
+      <select className="select-versions">{optionsRender()}</select>
+    </div>
   );
 }
 
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -10,7 +10,11 @@ import "./Home.css";
 
 function Home() {
   const CARDS_CONTENT = [
-    { title: "Office", image: office },
+    {
+      title: "Office",
+      image: office,
+      versiones: ["2019", "2016", "2013", "2010"],
+    },
     { title: "Chrome", image: chrome },
     { title: "Winrar", image: winrar },
     { title: "Vlc", image: vlc },
@@ -19,38 +23,29 @@ function Home() {
   const [appSelected, setAppSelected] = useState([]);
 
   const handlePressApp = (title) => {
-    setAppSelected((prev) => {
-      const newArray = prev;
-      /* if (newArray.includes(title)) {
-        return newArray.filter((element) => element !== title);
-      } else {
-        newArray.push(title);
-        console.log("newArray", newArray);
-        return newArray;
-      } */
-      if (!newArray.includes(title)) newArray.push(title);
-      return newArray;
-    });
-    console.log(appSelected);
+    setAppSelected((prev) =>
+      prev.includes(title)
+        ? prev.filter((element) => element !== title)
+        : prev.concat(title)
+    );
   };
 
   return (
     <React.Fragment>
-      <div>
-        <h1 className="title">AutoInstall</h1>
-        <div className="cards-list">
-          {CARDS_CONTENT.map((item, index) => (
-            <Card
-              {...item}
-              key={index}
-              handlePressApp={handlePressApp}
-              selected={appSelected.includes(item.title)}
-            />
-          ))}
-        </div>
-        <div className="btn-container">
-          <button className="bnt-install">{`Instalar (${appSelected.length})`}</button>
-        </div>
+      <h1 className="title">AutoInstall</h1>
+      <div className="cards-list">
+        {CARDS_CONTENT.map((item, index) => (
+          <Card
+            {...item}
+            key={index}
+            handlePressApp={handlePressApp}
+            selected={appSelected.includes(item.title)}
+            appSelected={appSelected}
+          />
+        ))}
+      </div>
+      <div className="btn-container">
+        <button className="bnt-install">{`Instalar (${appSelected.length})`}</button>
       </div>
     </React.Fragment>
   );
